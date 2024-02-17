41e5cff302287ed0e88374bb53c1b2d72b19d6e6
maroHaru <134841618+maroHaru@users.noreply.github.com>
2024-02-17 10:13:54 +0000

    [Add] Confirmation question

--- app.js
+++ app.js
@@ -48,11 +48,20 @@
 
 // alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
+// let i;
+// let num = 0;
+
+// for (i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
 let i;
 let num = 0;
 
 for (i = 1; i < 11; i++){
-  num = num + i;
+  num = num / i;
 }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+alert('1から10まで割り算した結果は' + num + 'です');
