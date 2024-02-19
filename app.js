e20a260419f509d1f2aacead5e6ad1dc8304eadc
maroHaru <134841618+maroHaru@users.noreply.github.com>
2024-02-19 10:40:47 +0000

    [Add] Using functions in Javascript

--- app.js
+++ app.js
@@ -57,11 +57,45 @@
 
 // alert('1から10まで足し算した結果は' + num + 'です');
 
-let i;
-let num = 0;
+// let i;
+// let num = 0;
 
-for (i = 1; i < 11; i++){
-  num = num / i;
+// for (i = 1; i < 11; i++){
+//   num = num / i;
+// }
+
+// alert('1から10まで割り算した結果は' + num + 'です');
+
+// let alertString;
+
+// alertString = addString("WebCamp");
+
+// alert(alertString);
+
+// function addString(strA) {
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+
+// let promptStr = prompt('何か好きな文字を入力してください。');
+
+// alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+alert('あなたの選んだ手は' + user_hand + 'です。');
+
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  } else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  } else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+
+  return js_hand;
 }
-
-alert('1から10まで割り算した結果は' + num + 'です');
